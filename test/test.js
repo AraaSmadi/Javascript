2837cacedc01d0ba1f724745237cc91b889f58c5
AraaSmadi <100359539+AraaSmadi@users.noreply.github.com>
2022-03-24 19:56:33 +0300

    main

---
+++ test/test.js
@@ -0,0 +1,32 @@
+/*var num1 = prompt("please enter the first number :: ");
+var op = prompt("please enter the Operation (+ , - , * , /) :: ");
+var num2 = prompt("please enter the secound number :: ");
+
+if (op == "+") {
+    var sum = parseInt(num1) + parseInt(num2);
+    document.write(num1 + "+" + num2 + "=", sum);
+
+} else if (op == "-") {
+    var sub = parseInt(num1) - parseInt(num2);
+    document.write(num1 + "-" + num2 + "=", sub);
+} else if (op == "*") {
+    var multiply = parseInt(num1) * parseInt(num2);
+    document.write(num1 + "*" + num2 + "=", multiply);
+} else if (op == "/") {
+    var dividing = parseInt(num1) / parseInt(num2);
+    document.write(num1 + "/" + num2 + "=", dividing);
+} else {
+    document.write("The entry is wrong!");
+}
+let value = 0.22;
+switch (value) {
+    case 0.22:
+        document.write(value);
+        break;
+    default:
+        document.write("not");
+}
+let arr = [10, 20, 30];
+for (i = 0; i < arr.length; i++) {
+    arr[0] = document.write(arr[i] + "<br>");
+}*/
